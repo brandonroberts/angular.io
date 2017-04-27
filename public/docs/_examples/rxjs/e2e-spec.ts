4f280056764e072efe4c4d1bb4d09dc432f72e56
Brandon Roberts <robertsbt@gmail.com>
2017-04-26 19:43:47 -0500

    Added tests

--- public/docs/_examples/rxjs/e2e-spec.ts
+++ public/docs/_examples/rxjs/e2e-spec.ts
@@ -1,12 +1,34 @@
 'use strict'; // necessary for es6 output in node
 
-import { browser
-/*, element, by, ElementFinder*/
-} from 'protractor';
+import { browser, element, by, ElementFinder } from 'protractor';
 
 describe('RxJS', function () {
+  let page: any;
+
+  function getPage() {
+    return {
+      findHrefs: () => element.all(by.css('my-app a')),
+      findAddHero: () => element(by.linkText('Add Hero')),
+      findHeroes: () => element(by.linkText('Heroes')),
+      findHeroCounter: () => element(by.linkText('Hero Counter')),
+
+      findMessageLog: () => element(by.css('message-log')),
+      findHeroList: () => element(by.css('ul.items')),
+      findHeroListItems: () => element.all(by.css('ul.items li'))
+    };
+  }
 
   beforeAll(function () {
     browser.get('');
+  });
+
+  beforeEach(() => {
+    page = getPage();
+  });
+
+  it('should have 10 heroes', () => {
+    const heroes = page.findHeroListItems();
+
+    expect(heroes.length).toBe(10);
   });
 });
